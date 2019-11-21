b0b53479b01306b11e3edcedcbe97f56c031c530
LASSE <lassehovden@gmail.com>
2019-11-21 16:27:33 +0100

    added linking to timepage

--- pages/time.page.js
+++ pages/time.page.js
@@ -10,29 +10,29 @@ const timePage={
          <h1>Velg Ønsket Tidspunkt</h1>
       </div>
       <div class="timetable">
-         <router-link to="/startPage" tag="div" class="time"><p>7:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>8:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>9:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>8:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>9:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>10:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>10:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>11:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>11:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>12:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>12:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>13:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>13:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>14:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>14:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>15:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>15:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>16:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>16:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>17:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>17:30</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>18:00</p></router-link>
-         <router-link to="/startPage" tag="div" class="time"><p>18:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>7:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>8:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>9:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>8:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>9:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>10:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>10:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>11:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>11:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>12:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>12:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>13:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>13:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>14:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>14:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>15:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>15:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>16:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>16:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>17:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>17:30</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>18:00</p></router-link>
+         <router-link to="/contactinfo" tag="div" class="time"><p>18:30</p></router-link>
       </div>
    </div>
    `
